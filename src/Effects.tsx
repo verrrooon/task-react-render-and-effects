24e37831335c6dd97719231a834f0fa00c0d28a1
verrrooon <verrrooon@yandex.ru>
2022-05-18 17:34:40 +0300

    Задание по рендерингу

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,27 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+
+    const callback = (msg: number) => {
+        setMessage(msg);
+    };
+
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+
+        const res = () => {
+            unsubscribe(props.sourceId, callback);
+            setMessage(-1);
+        };
+
+        return res;
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
